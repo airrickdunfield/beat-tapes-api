8027bf5600d8cf417f876d6593c60f5a446d9b9f
Airrick Dunfield <hello@airrickdunfield.com>
2025-04-01 16:04:20 -0700

    Adds pg

--- db.js
+++ db.js
@@ -1,17 +1,11 @@
-const USER = process.env.DB_USER;
-const PASSWORD = process.env.DB_PASSWORD;
-const HOST = process.env.DB_HOST;
-const PORT = process.env.DB_PORT;
-const DATABASE = process.env.DB_DATABASE;
-    
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: USER,
-  password: PASSWORD,
-  host: HOST,
-  port: PORT, // default Postgres port
-  database:DATABASE
+  user: PGUSER,
+  password: PGPASSWORD,
+  host: PGHOST,
+  database:PGDATABASE
 });
 
 module.exports = {
