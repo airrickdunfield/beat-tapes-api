eaee4856a4724aeded81761dad0dcdc464e346ea
Airrick Dunfield <hello@airrickdunfield.com>
2025-04-02 14:36:23 -0700

    Adds db updates

--- db.js
+++ db.js
@@ -2,14 +2,14 @@ const mysql = require('mysql2');
 
 // Create a connection pool
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER, // Use Railway or environment variables
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQLPORT,
-    waitForConnections: true,
-    connectionLimit: 10, // Maximum number of connections in the pool
-    queueLimit: 0, // Unlimited queueing
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: require('fs').readFileSync(process.env.DB_SSL_CA) // Load the CA certificate
+  }
 });
 
 // Export the pool for use in other parts of the app
