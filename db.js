8a9cada4437a9ebb21aef5fcd9708aa9195e33ca
Airrick Dunfield <hello@airrickdunfield.com>
2025-04-02 17:00:31 -0700

    Adds db changes=

--- db.js
+++ db.js
@@ -11,10 +11,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  // port: process.env.DB_PORT,
-  ssl: {
-    ca: fs.readFileSync(process.env.DB_SSL_CA), // Load the SSL certificate
-  },
+  port: process.env.DB_PORT
 });
 
 // Connect to the database
